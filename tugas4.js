178884f02b2777d5632dc06d7d54c54cb8495c04
bagas28 <bagasrizki700@gmail.com>
2024-06-27 15:09:10 +0700

    fix task 4

--- tugas4.js
+++ tugas4.js
@@ -2,7 +2,11 @@ function terminalTransjakarta() {
   for (let busNumber = 1; busNumber <= 10; busNumber++) {
     if (busNumber <= 6) {
       console.log(`Bus transjakarta ${busNumber} beroperasi dengan baik.`);
-    } else {
+    } else if (busNumber == 7) {
+      console.log(`Bus transjakarta ${busNumber} sedang tidak beroperasi.`);
+    } else if (busNumber == 8) {
+      console.log(`Bus transjakarta ${busNumber} sedang Lembur.`);
+    } else if (busNumber >= 9) {
       console.log(`Bus transjakarta ${busNumber} sedang tidak beroperasi.`);
     }
   }

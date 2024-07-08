5cac1e0f110152888767e59d6ae8f4f09752b9ae
bagas28 <bagasrizki700@gmail.com>
2024-07-08 16:22:38 +0700

    first commit

---
+++ Pertemuan-7-vue/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <!-- <nav>
+    <RouterLink to="/">Input Ingredient</RouterLink>
+    <RouterLink to="/cetak">Cetak Tagihan</RouterLink>
+  </nav> -->
+  <RouterView />
+</template>
---
+++ Pertemuan-7-vue/src/views/PrintTagihan.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { useRoute, useRouter } from "vue-router";
+import { computed } from "vue";
+
+const route = useRoute();
+const router = useRouter();
+const totalAmount = route.query.totalAmount;
+const totalPrice = route.query.totalPrice;
+const namaCustomer = route.query.customer;
+
+console.log(route.query.totalPrice);
+</script>
+
+<template>
+  <div>
+    <h1>Payment</h1>
+    <p>
+      Total pembayaran yang harus dibayarkan {{ namaCustomer }} dari
+      {{ totalAmount }} items sebesar <br />
+      Rp. {{ totalPrice }},00
+    </p>
+    <button type="button" @click="router.back()">Back</button>
+  </div>
+</template>
+
+<style scoped>
+div {
+  border: 2px solid black;
+  border-radius: 5px;
+  padding: 10px;
+}
+</style>

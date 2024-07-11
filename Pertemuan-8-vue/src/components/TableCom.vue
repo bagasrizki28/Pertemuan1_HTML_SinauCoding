64258a5cfc16a95012facbd06157ef009a48bd5d
bagas28 <bagasrizki700@gmail.com>
2024-07-12 06:55:14 +0700

    adding tugas 8

---
+++ Pertemuan-8-vue/src/components/TableCom.vue
@@ -0,0 +1,36 @@
+<script setup>
+defineProps({
+  dataTable: {
+    type: Object,
+    default: {
+      header: ["ID", "Nama", "Position", "No. Telp", "Email"]
+    },
+  },
+});
+
+const emit = defineEmits(["showData"]);
+</script>
+
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th v-for="(data, index) in dataTable.header" :key="index">
+          {{ data }}
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="data in dataTable.data" :key="data.id">
+        <td>{{ data.id }}</td>
+        <td>{{ data.nama }}</td>
+        <td>{{ data.position }}</td>
+        <td>{{ data.no_telp }}</td>
+        <td>{{ data.email }}</td>
+        <td>
+          <button type="Submit" @click="emit('showData', data)">Show Data</button>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
---
+++ Pertemuan-8-vue/src/views/TableView.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { reactive } from "vue";
+import TableComponent from "../components/TableCom.vue";
+import Header from "../json/Header.json";
+import User from "../json/User.json";
+
+const tableData = reactive({
+  header: Header,
+  data: User,
+});
+
+const ShowDatahandle = (data) => {
+  alert(
+    `Nama ${data.nama}, Posisi ${data.position}, No Telp. ${data.no_telp} , Email ${data.email} `
+  );
+};
+</script>
+
+<template>
+  <div>
+    <h3>Table View</h3>
+    <TableComponent :dataTable="tableData" @showData="ShowDatahandle" />
+  </div>
+</template>
